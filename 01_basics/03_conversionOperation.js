4054cd1200d41e1fc0aab09b96dbfb6ecc3fd997
SupriyaMohare <117314060+SupriyaMohare@users.noreply.github.com>
2024-07-19 10:51:21 +0000

    datatype_conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+/*let score = "33asbs";
+
+
+console.log(typeof score);
+console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);//33
+
+let sc = null;
+console.log(typeof sc);
+console.log( Number(sc));*/
+
+//if we convert it into number
+//"33"=>33
+//"33abcd"=>NAN
+//true =>1
+
+
+
+let isLoggedIn = " ";
+let boolisLoggedIn =Boolean(isLoggedIn);
+console.log(boolisLoggedIn);
+
+// to Boolean
+//1 => true 0 =>false
+//" "=> true
+//""=>false;
+//"dfghjk"=>true
