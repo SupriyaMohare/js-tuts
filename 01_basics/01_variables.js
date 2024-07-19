252cad13456560155b10396c284d24ceaf9a5759
SupriyaMohare <117314060+SupriyaMohare@users.noreply.github.com>
2024-07-19 10:23:33 +0000

    variables nd datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 1245342
+let accountEmail = "hitesh@gmail.com"
+var accountPasswords = "12345"
+accountcity = "Mumbai"
+
+
+let accountstate;
+// accountId=2 //not allowed
+
+accountEmail="hc@hd.com"
+accountPasswords = "334542"
+accountcity ="Delhi"
+
+console.log(accountId);
+
+
+console.table([accountId,accountEmail,accountPasswords,accountcity,accountstate])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict";//treat all js code as newer version
+
+//alert(3+3)   //on browser(on console) we get popup. but here error 
+
+let name ="hitesh"
+let age = 18
+let isLoggedIn = false
+let state =null
+
+// preemetive datatypes
+//number ,String, boolean,  null -standalone value  (empty other than 0),  
+//undefined-variable is decclared but value is not assign 
+//symbol = uniquesness 
+
+
+//object
+console.log(typeof "Suppi");
+console.log(typeof null);
+console.log(typeof undefined);
