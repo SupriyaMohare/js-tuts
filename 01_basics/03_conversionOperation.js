551255d2ca83bdb856b691e30001ae075b5dd7b5
SupriyaMohare <117314060+SupriyaMohare@users.noreply.github.com>
2024-07-20 09:45:03 +0000

    comparisons

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -21,11 +21,41 @@ console.log( Number(sc));*/
 
 
 let isLoggedIn = " ";
-let boolisLoggedIn =Boolean(isLoggedIn);
-console.log(boolisLoggedIn);
+// let boolisLoggedIn =Boolean(isLoggedIn);
+// console.log(boolisLoggedIn);
 
 // to Boolean
 //1 => true 0 =>false
 //" "=> true
 //""=>false;
 //"dfghjk"=>true
+
+//*************Operations */
+
+// let  value=2;
+// let negvalue=-value;
+// console.log(negvalue);
+
+
+
+// console.log(2+2);
+// console.log(2**2);
+
+// let str1 ="hello"
+// let str2 =" Suppu"
+
+// let str3 =str1+str2;
+// console.log(str3);
+
+// console.log(2+2);
+
+let s ="1"
+let n = 2;
+let ans=s+n;
+
+// console.log( ans);
+// console.log(typeof ans);
+
+
+console.log("1"+2+2);
+
---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,17 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2==1);
+
+
+// console.log("2">1);
+
+// console.log(null > 0);
+// console.log(null==0);
+// console.log(null>=0);
+
+
+let x ="10"
+let y=10
+
+console.log(x==y);
+console.log(x===y);
